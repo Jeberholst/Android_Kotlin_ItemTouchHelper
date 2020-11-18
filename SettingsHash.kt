b42c19f511f8a1b4f1fba7dda8c275ec16d33bf0
Joakim Eberholst <47057279+Jeberholst@users.noreply.github.com>
2020-11-18 21:30:05 +0100

    Create SettingsHash.kt

---
+++ SettingsHash.kt
@@ -0,0 +1,10 @@
+val itemTouchHelperSettings: HashMap<String, Any> = hashMapOf(
+    "SwipeThreshold" to 0.3f,
+    "SwipeEscapeVelocity" to 3f,
+    "SwipeLeftIcon" to R.drawable.something OR "",
+    "SwipeRightIcon" to  R.drawable.something OR "",
+    "SwipeUpIcon" to R.drawable.something OR "",
+    "SwipeDownIcon" to  R.drawable.something OR "",
+    "ItemViewHeightReduction" to 60,
+    "ItemViewWidthReduction" to 60
+)
